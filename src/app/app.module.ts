0d54656f923ed7c5fa3b91152eea24e8a4fca55f
Minu-S <minusekhar002@gmail.com>
2020-06-01 14:14:28 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
