78ee72cb93e74a191a5fd725ba4adf1736821b81
Minu-S <minusekhar002@gmail.com>
2020-06-01 17:28:03 +0530

    1 file modified

---
+++ src/app/firebase.service.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+import { FireBaseService} from './firebase.servce';
+describe('FireBaseService', () => {
+let service: FirebaseService;
+beforeEach(() => {
+  TestBed.configureTestingModule({});
+  service = TestBed.inject(FirebaseService);
+});
+it('should be created', () => {
+  expect(service).toBeTruthy();
+});
+});
+
