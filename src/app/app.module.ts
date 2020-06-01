70e2e94873869cec16bf6491fa5597101b472693
Minu-S <minusekhar002@gmail.com>
2020-06-01 17:33:32 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FirebaseService } from './firebase.service';
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule, FormsModule,AngularFireModule.initializeApp(environment.firebase), AngularFirestoreModule],
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule,AngularFireModule.initializeApp(environment.firebase), AngularFirestore, AngularFireAuthModule],
   declarations: [ AppComponent, HelloComponent, routingComponents, PageNotFoundComponent,],
   bootstrap:    [ AppComponent ],
   providers: [FirebaseService]
