22d70d32a9fd3bf6d9e1ac5ceecb278c69883a2e
Minu-S <minusekhar002@gmail.com>
2020-06-01 13:54:24 +0530

    1 file modified, 3 added

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA9_FzP6iW8eIA45dKowpNkWr_4Ry0tSHI",
+    authDomain: "login-form-bd4a4.firebaseapp.com",
+    databaseURL: "https://login-form-bd4a4.firebaseio.com",
+    projectId: "login-form-bd4a4",
+    storageBucket: "login-form-bd4a4.appspot.com",
+    messagingSenderId: "238119034297",
+    appId: "1:238119034297:web:c07cef4423ff68a4862a68",
+    measurementId: "G-JEKS1JRPC1"
+  }
+};
