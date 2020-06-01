83e7604fbfc9d6bdc34a676f476e03623e86bac4
Minu-S <minusekhar002@gmail.com>
2020-06-01 17:14:10 +0530

    3 files modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/firestore';
-@Injectable()
+import { AngularFirestore } from '@angular/fire/firestore';
+@Injectable({
+  providedIn: 'root'
+})
 export class FirebaseService {
 
-  constructor() { }
-
-}
+  constructor(public db:AngularFirestore) { }
+    createUser(value) {
+      console.log("creating user..." + value.userName);
+      return this .db.collection('users').add({
+        name: value.userName,
+        dob: value.dob,
+        role: value.role,
+        email: value.email
+      });
+    }
+  }
