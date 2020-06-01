8857cb9d5a0b325d304b8e9135c29232bf58e0a7
Minu-S <minusekhar002@gmail.com>
2020-06-01 15:56:33 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire';
-import { AngularStorageModule } from '@angular/fire/storage';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FirebaseService } from './firebase.service';
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule, FormsModule,AngularFireModule.initializeApp(environment.firebase), AngularStorageModule],
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule,AngularFireModule.initializeApp(environment.firebase), AngularFireStorage],
   declarations: [ AppComponent, HelloComponent, routingComponents, PageNotFoundComponent,],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [FirebaseService]
 })
 export class AppModule { }
